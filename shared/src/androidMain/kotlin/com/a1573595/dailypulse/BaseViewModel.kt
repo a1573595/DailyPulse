f3232dbd8c4b0122b735266fbc30268a2e2a017c
a1573595 <a1573595@gmail.com>
2024-07-23 13:52:09 +0800

    BaseViewModel with Coroutine

---
+++ shared/src/androidMain/kotlin/com/a1573595/dailypulse/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package com.a1573595.dailypulse
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual open class BaseViewModel : ViewModel() {
+    actual val scope: CoroutineScope = viewModelScope
+}
---
+++ shared/src/commonMain/kotlin/com/a1573595/dailypulse/BaseViewModel.kt
@@ -0,0 +1,8 @@
+package com.a1573595.dailypulse
+
+import kotlinx.coroutines.CoroutineScope
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+expect open class BaseViewModel {
+    val scope: CoroutineScope
+}
